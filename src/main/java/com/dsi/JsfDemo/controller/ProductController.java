395659c74e7ca6dc186d40fb706603ee1b043486
lubna40 <lubu7035@gmail.com>
2021-04-01 13:41:39 +0600

    Paste files

---
+++ src/main/java/com/dsi/JsfDemo/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.dsi.JsfDemo.controller;
+
+import com.dsi.JsfDemo.model.Product;
+import com.dsi.JsfDemo.repository.ProductRepository;
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope(value = "session")
+@Component(value = "productController")
+@ELBeanName(value = "productController")
+@Join(path = "/product", to = "/product-form.jsf")
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    private Product product = new Product();
+
+    public String save() {
+        productRepository.save(product);
+        product = new Product();
+        return "/product-list.xhtml?faces-redirect=true";
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (id, name, price) values (1000, 'rice', 25);
+INSERT INTO product (id, name, price) values (1001, 'juice', 100);
+INSERT INTO product (id, name, price) values (1002, 'rice', 50);
+INSERT INTO product (id, name, price) values (1003, 'oil', 100);
+INSERT INTO product (id, name, price) values (1004, 'ata', 22);
+INSERT INTO product (id, name, price) values (1005, 'chiken', 99);
